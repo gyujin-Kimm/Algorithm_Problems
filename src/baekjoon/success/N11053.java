c0d436e0099b3a4335d0f9c05d5299a31ddf731e
gyujin-kimm <kjkj268@naver.com>
2024-02-23 23:59:14 +0900

    가장 긴 증가하는 부분 수열 - DP, Binary Search

--- src/baekjoon/success/N11053.java
+++ src/baekjoon/success/N11053.java
@@ -26,3 +26,47 @@ public class Main {
         System.out.println(max);
     }
 }
+
+// 이진 탐색
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+//     static int N;
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//         N = Integer.parseInt(br.readLine());
+//         int[] lis = new int[N];
+//         int[] A = Arrays.stream(br.readLine().split(" "))
+//                 .mapToInt(Integer::parseInt)
+//                 .toArray();
+//         lis[0] = A[0];
+
+//         int len = 1;
+//         for (int i = 1; i < N; i++) {
+//             if (lis[len - 1] < A[i]) {
+//                 lis[len++] = A[i];
+//             }
+//             else if (lis[len - 1] > A[i]) {
+//                 int idx = binarySearch(lis, 0, len - 1, A[i]);
+//                 lis[idx] = A[i];
+//             }
+//         }
+
+//         System.out.println(len);
+//     }
+//     private static int binarySearch(int[] lis, int left, int right, int target) {
+//         int mid;
+//         while (left < right) {
+//             mid = ((right + left) % N) / 2;
+//             if (lis[mid] < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+//         return right;
+//     }
+// }
